dfed9a62029041d5bfc871fd4e452db4c7d26e6a
Cristian Gomez <gomez18cristian@gmail.com>
2021-05-25 19:40:02 -0500

    two-fer

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const twoFer = (a = 'you') => {
-  return(`One for ${a}, one for me.`)
+
+export const twoFer = (name = 'you') => {
+  return(`One for ${name}, one for me.`)
 };
